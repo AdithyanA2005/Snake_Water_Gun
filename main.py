2c0afc3588720242c4a4000ec5de925d22abeec0
AdithyanA <deepaadithyan56@gmail.com>
2020-09-26 07:16:28 +0530

    program transfered under while loop

--- main.py
+++ main.py
@@ -1,98 +1,94 @@
-# SNAKE WATER GUN
-# In this game the player will play with the computer'Computer'
-# and the user 'Player'
-
-
-
+"""
+##############################################################################
+SNAKE WATER GUN:-                                                            #
+#################                                                            #
+                                                                             #
+                #######################################################      #
+                 In this game the Player will play with the computer         #
+                 'Computer' and the user 'Player'                            #
+                #######################################################      #
+                                                                             #
+##############################################################################
+"""
 
 # Importing Modules needed for the game
 import random
 
 
+# FUNCTION'S
 # Function's needed for the program
-def playRule(Computer, Player):
-
+def playRule(computer, player):
     # 1) If the computer and the player choose same nothing will happen
-    if Computer == Player:
+    if computer == player:
         return None
 
     # 2) If the computer chooses 'Snake'
-    elif Computer == 's':
-
+    elif computer == 's':
         # and player choose 'Water', Computer got point
-        if Player == 'w':
+        if player == 'w':
             return False
-
         # or Player choose 'Gun', Player got point
-        elif Player == 'g':
+        elif player == 'g':
             return True
 
     # 3) If computer chooses 'Water'
-    elif Computer == 'w':
-
+    elif computer == 'w':
         # and player choose 'Gun', Computer got point
-        if Player == 'g':
+        if player == 'g':
             return False
-
         # or player choose 'Snake', Player got point
-        elif Player == 's':
+        elif player == 's':
             return True
 
     # 4) If compute chooses 'Gun'
-    elif Computer == 'g':
-
+    elif computer == 'g':
         # and player choose 'Snake', Computer got point
-        if Player == 's':
+        if player == 's':
             return False
-
         # or player choose 'Water', Player got point
-        elif Player == 'w':
+        elif player == 'w':
             return True
 
 
+# PROGRAM START HERE:
+# To run the code infinitely many times
+while True:
+    # First the turn is for the computer and we will print
+    print("\nComputer Turn: Snake(s) Water(w) or Gun(g)")
 
+    # We use the 'Random' module to make the computer choose "'s' or 'w' or 'g'"  randomly the 'randint' provide number
+    # from first number to the last number
+    RandomNumber = random.randint(1, 3)
 
-# START
-# First the turn is for the computer and we will print
-print("\nComputer Turn: Snake(s) Water(w) or Gun(g)")
+    # Now we will assign the numbers to snake or water or gun
+    if RandomNumber == 1:  # If random number is 1 the computer choose for 'Snake'
+        Computer = 's'
 
+    elif RandomNumber == 2:  # If random number is 2 the computer choose for 'Water'
+        Computer = 'w'
 
-# We use the 'Random' module to make the computer choose "'s' or 'w' or 'g'"  randomly
-# the 'randit' provide number from first number to the last number
-RandomNumber = random.randint(1, 3)
-if RandomNumber == 1:  # If random number is 1 the computer choose for 'Snake'
-    Computer = 's'
+    elif RandomNumber == 3:  # If random number is 3 the computer choose for 'Gun'
+        Computer = 'g'
 
-elif RandomNumber == 2:  # If random number is 2 the computer choose for 'Water'
-    Computer = 'w'
+    # After the computer have choose we will tell the user that the comuter had choosed
+    print(f"Computer Choose{Computer}")
 
-elif RandomNumber == 3:  # If random number is 3 the computer choose for 'Gun'
-    Computer = 'g'
+    # After the computer it is the players turn to put the input
+    Player = input("\nYour Turn: Snake(s) Water(w) or Gun(g)")
 
-# After the computer have choosed we will tell the user that the comuter had choosed
-print(f"Computer Choosed")
+    # We are calling the function we created earlier in 'result'
+    result = playRule(Computer, Player)
 
-# After the computer it is the players turn to put the input
-Player = input("\nYour Turn: Snake(s) Water(w) or Gun(g)")
+    # We will also show the selection of the Computer and the Player to the user
+    print(f"\nComputer Choose: {Computer}")
+    print(f"You Choose: {Player}\n")
 
+    # Now we are creating other if case to declare who had win the game
+    if result is None:  # if The user and the computer are in tie('None')
+        print("The game was a tie match")
 
-# We are calling the function we created earlier in 'result'
-result = playRule(Computer, Player)  
+    elif result:  # If the player win
+        print("You Win")
 
-# We will also show the selection of the Computer and the Player to the user
-print(f"\nComputer Choosed: {Computer}")
-print(f"You Choosed: {Player}\n")
-
-
-# Now we are creating other if case to declare who had winned the game 
-
-# if The user and the computer are in tie('None')
-if result == None:
-    print("The game was a tie match")
-
-# If the player scored the point('True')
-elif result:
-    print("You Win")
-
-else:
-    print('You loose')
+    else:  # If the player loose
+        print('You loose')
